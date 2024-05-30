dab5359670387f0023ef97df39d3bfee794c32cd
Luca Lanzo <luca.lanzo21@gmail.com>
2024-05-30 21:25:04 +0200

    create repository for CRUD operations and searches for status and position

--- Backend/src/main/java/com/lucalanzo/data/repository/JobInterviewRepository.java
+++ Backend/src/main/java/com/lucalanzo/data/repository/JobInterviewRepository.java
@@ -1,4 +1,13 @@
 package com.lucalanzo.data.repository;
 
-public class JobInterviewRepository {
+import com.lucalanzo.data.resource.ApplicationStatus;
+import com.lucalanzo.data.resource.JobInterview;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public abstract class JobInterviewRepository implements MongoRepository<JobInterview, String> {
+    public abstract List<JobInterview> findByStatus(ApplicationStatus status);
+
+    public abstract List<JobInterview> findByPosition(String position);
 }
