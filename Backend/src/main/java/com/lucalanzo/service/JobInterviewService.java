6b1968238485dcfc541a0e8a2123c8d9b1a2de27
Luca Lanzo <luca.lanzo21@gmail.com>
2024-05-30 21:29:19 +0200

    create CRUD operations as a connection between the controller and the repository

--- Backend/src/main/java/com/lucalanzo/service/JobInterviewService.java
+++ Backend/src/main/java/com/lucalanzo/service/JobInterviewService.java
@@ -1,7 +1,45 @@
 package com.lucalanzo.service;
 
+import com.lucalanzo.data.repository.JobInterviewRepository;
+import com.lucalanzo.data.resource.JobInterview;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@AllArgsConstructor
 public class JobInterviewService {
+    private final JobInterviewRepository repository;
+
+    public List<JobInterview> getAllJobInterviews() {
+        return repository.findAll();
+    }
+
+    public Optional<JobInterview> getJobInterview(String id) {
+        return repository.findById(id);
+    }
+
+    public JobInterview createJobInterview(JobInterview jobInterview) {
+        return repository.insert(jobInterview);
+    }
+
+    public boolean changeJobInterview(String id, JobInterview jobInterview) {
+        if (!repository.existsById(id)) return false;
+
+        jobInterview.setId(id);
+        repository.save(jobInterview);
+
+        return true;
+    }
+
+    public boolean deleteJobInterview(String id) {
+        if (!repository.existsById(id)) return false;
+
+        repository.deleteById(id);
+
+        return true;
+    }
+
 }
