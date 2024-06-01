9be328478eebdc2e8298568e13dcdb39af1a9de7
Luca Lanzo <luca.lanzo21@gmail.com>
2024-06-01 19:41:49 +0200

    edit file structure

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'job-interview-tracker-frontend' title`, () => {
+  it(`should have the 'frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('job-interview-tracker-frontend');
+    expect(app.title).toEqual('frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, job-interview-tracker-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
   });
 });
